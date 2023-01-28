e3b3d2ebbff1476709708f243681cf5d1905e6f0
SonJinHYo <SonJinHYo@github.com>
2023-01-28 18:45:23 +0900

    check_clear

--- users/views.py
+++ users/views.py
@@ -178,9 +178,6 @@ class GithubLogIn(APIView):
 
 
 class KakaoLogIn(APIView):
-    def check():
-        pass
-
     def post(self, request):
         try:
             code = request.data.get("code")
